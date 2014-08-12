cf9d2e79e2054152b261e2a261dba3561f4be0eb
sdkfbsdjhk@sdjnklf.com <sdkfbsdjhk@sdjnklf.com>
2014-08-12 12:53:33 +0300

    sdfas

--- Expose/app/urls.py
+++ Expose/app/urls.py
@@ -4,6 +4,7 @@ from app import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
+    url(r'home$', views.home, name='home'),
     url(r'login$', views.login, name='login'),
     url(r'specific$', views.specific, name = 'specific'),
 )
